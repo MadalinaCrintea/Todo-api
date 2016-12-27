f2a3f336a168f98e2deae5a0ed4de7d69514331c
Madalina Crintea <madalina.crintea@gmail.com>
2016-12-27 21:26:46 +0200

    Add ?completed filter

--- server.js
+++ server.js
@@ -13,10 +13,21 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
+//GET /todos
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	res.json(filteredTodos);
 });
 
+//GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
