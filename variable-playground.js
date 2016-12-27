932afc29ee907dbc8f3c6b42396b66bdaf8c7fa3
Madalina Crintea <madalina.crintea@gmail.com>
2016-12-27 21:28:13 +0200

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,21 @@
+/*var person = {
+	name: 'Andrew',
+	age:21
+};
+
+function updatePerson (obj) {
+	obj.age = 24;
+}
+
+updatePerson(person);
+console.log(person);*/
+
+var grades = [15, 88];
+
+function addGrade (array) {
+	array.push(55);
+	debugger;
+}
+
+addGrade(grades);
+console.log(grades);
