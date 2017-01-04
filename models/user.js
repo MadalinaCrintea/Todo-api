9dd908c3d2a10b75503cb459b69af32e7150c722
Madalina Crintea <madalina.crintea@gmail.com>
2017-01-04 15:00:35 +0200

    Convert email to lower case

--- models/user.js
+++ models/user.js
@@ -15,5 +15,14 @@ module.exports = function (sequelize, DataType) {
 				len: [7, 100]
 			}
 		}
+	}, {
+		hooks: {
+			beforeValidate: function (user, options) {
+				//user.email
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
+				}
+			}
+		}
 	});
 };
